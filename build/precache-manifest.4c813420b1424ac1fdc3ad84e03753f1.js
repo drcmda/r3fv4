4bf78a5fad67864caed095dcbe54d13d94da9e42
Paul Henschel <drcmda@gmail.com>
2020-01-10 13:45:12 +0100

    ...

---
+++ build/precache-manifest.4c813420b1424ac1fdc3ad84e03753f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428ac4ed4c2e0169d353c7b8e2999cce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66873fb2864d0052ae18",
+    "url": "/static/css/main.7cd33b90.chunk.css"
+  },
+  {
+    "revision": "cb47bab687d77e15d893",
+    "url": "/static/js/2.6cf966c8.chunk.js"
+  },
+  {
+    "revision": "23b26446db404c4cd1d2ae5ba4f80a38",
+    "url": "/static/js/2.6cf966c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66873fb2864d0052ae18",
+    "url": "/static/js/main.c791eaa2.chunk.js"
+  },
+  {
+    "revision": "6d3291165d0378cbd6d0",
+    "url": "/static/js/runtime-main.bf6100d6.js"
+  }
+]);
---
+++ readme.md
@@ -0,0 +1,3 @@
+what's new: https://github.com/react-spring/react-three-fiber/blob/master/whatsnew.md
+demo: https://r3fv4.surge.sh
+codesandbox: https://codesandbox.io/s/amazing-kirch-sbf2i
